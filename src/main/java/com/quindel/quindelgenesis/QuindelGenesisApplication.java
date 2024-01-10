2a93800a41748108bbff0bb480e2ff21210b9edf
Germán Hernández López <inggermantics@gmail.com>
2024-01-10 14:00:13 -0600

    feature(QUINDLE-001) Start project
    - Add init project

---
+++ src/main/java/com/quindel/quindelgenesis/QuindelGenesisApplication.java
@@ -0,0 +1,13 @@
+package com.quindel.quindelgenesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuindelGenesisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuindelGenesisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quindel/quindelgenesis/QuindelGenesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quindel.quindelgenesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuindelGenesisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
